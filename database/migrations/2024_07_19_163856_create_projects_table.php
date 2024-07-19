e53e8ee9c28f2dc88952278d9405b39812bcf359
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-19 23:51:15 +0200

    feate: add migrate for create project table

--- database/migrations/2024_07_19_163856_create_projects_table.php
+++ database/migrations/2024_07_19_163856_create_projects_table.php
@@ -13,6 +13,29 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            /*
+-nome progetto
+-descrizione
+-data inizio
+-data fine
+-stato
+-budget
+-manager progetto
+-cliente
+-priorità
+-creato il 
+-aggironato il
+ */
+            $table->string('name', 50)->unique();
+            $table->text('description')->nullable();
+            $table->date('project_start_date')->nullable();
+            $table->date('project_end_date')->nullable();
+            $table->string('status', 30)->nullable();
+            $table->decimal('budget', 15, 2)->nullable();
+            $table->string('project_manager', 50)->nullable();
+            $table->string('customer', 50)->nullable();
+            $table->timestamp('creation_date')->nullable();
+            $table->timestamp('update_date')->nullable();
             $table->timestamps();
         });
     }
