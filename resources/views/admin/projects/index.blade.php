7b65a29ff8297cb4fe7381c597bc43bbf2bfdd54
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-07-20 15:22:56 +0200

    feat: add table in admin\projects\index.blade.php

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,19 +1,42 @@
 @extends('layouts.app')
 @section('content')
     <div class="container py-4">
-        <h1 class="py-3">Projects List</h1>
         <div class="content">
-            @foreach ($projects as $project)
-                <ul class="list-unstyled">
-                    <li class="py-2">
-                        <a href="">
-                            <h4>{{ $project->name }}</h4>
-                        </a>
-                        <div>{{ $project->status }}</div>
-                    </li>
-                </ul>
-                <hr>
-            @endforeach
+            <div class="header-content d-flex justify-content-between align-items-center">
+                <h1 class="py-3">Projects List</h1>
+                <div>
+                    <button class="btn btn-primary">Crea nuovo</button>
+                </div>
+            </div>
         </div>
+        {{-- table projects --}}
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Project name</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Settings</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <th scope="row">{{ $project->id }}</th>
+                        <td> <a href="">
+                                <div>{{ $project->name }}</div>
+                            </a></td>
+                        <td>
+                            <div>{{ $project->status }}</div>
+                        </td>
+                        <td>
+                            <button class="btn btn-primary btn-sm">modifica</button>
+                            <button class="btn btn-secondary btn-sm">cancella</button>
+                        </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
     </div>
 @endsection
